1cf3d795da916c99943d7e0441d7f85143358c3d
alizaKhalidKhan <119021188+alizaKhalidKhan@users.noreply.github.com>
2024-03-03 17:16:56 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <!--fonts-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/brands.min.css" />
+
+    <!--custom css-->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!--swiper css-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+</head>
+<body>
+   <!-- header section starts-->
+    <section class="header">
+            <a href="index.php" class="logo">A&M Travel Agency</a>
+            <nav class="navbar">
+                <a href="index.php">Home</a>
+                <a href="about.php">About Us</a>
+                <a href="package.php">Packages</a>
+                <a href="book.php">Book Now</a>
+            </nav>
+            <div id="menu-btn" class="fas fa-bars"></div>
+
+    
+    </section>
+
+    <!-- header section ends-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!--footer starts-->
+    <section class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3>Quick Links</h3>
+                <a href="index.php"> <i class="fas fa-angle-right"></i> Home</a>
+                <a href="about.php"> <i class="fas fa-angle-right"></i> About Us</a>
+                <a href="package.php"> <i class="fas fa-angle-right"></i> Packages</a>
+                <a href="book.php"> <i class="fas fa-angle-right"></i> Book Now</a>
+            </div>
+            <div class="box">
+                <h3>Extra Links</h3>
+                <a href="#"> <i class="fas fa-angle-right"></i> Ask Questions </a>
+                <a href="#"> <i class="fas fa-angle-right"></i> About Us</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> Privacy Policy</a>
+                <a href="#"> <i class="fas fa-angle-right"></i> Terms Of Use</a>
+            </div>
+            <div class="box">
+                <h3>Contact Info</h3>
+                <a href="#"> <i class="fas fa-phone"></i> +92-73839-2222</a>
+                <a href="#"> <i class="fas fa-phone"></i> +92-73839-2223</a>
+                <a href="#"> <i class="fas fa-envelope"></i> AMtravels@gmail.com</a>
+                <a href="#"> <i class="fas fa-map"></i> Karachi , Pakistan</a>
+            </div>
+            <div class="box">
+                <h3>Follow us</h3>
+                <a href="#"> <i class="fab fa-facebook"></i>Facebook</a>
+                <a href="#"> <i class="fab fa-x-twitter"></i>Twitter</a>
+                <a href="#"> <i class="fab fa-instagram"></i>Instagram</a>
+                <a href="#"> <i class="fab fa-linkedin"></i>LinkedIn</a>
+            </div>
+        </div>
+
+
+    </section>
+    <!--footer ends-->
+<!--js link-->
+<script src="js/script.js"></script>
+
+<!--swiper js link-->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<body>
+</html>
